993e7b3d32b00235b219a6b10192d66668a4fb39
kiyan rahmati <kiyan.rahmati.ss@gmail.com>
2026-08-13 14:38:00 +0330

    Create TaskForm.vue

---
+++ vue-project/src/TaskForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <form class="task-form">
+        <input placeholder="enter your tsk ...">
+        <button type="submit"> add </button>
+    </form>
+</template>
+
+<style>
+.task-form {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 15px;
+}
+.task-form input {
+    flex: 1;
+    padding: 8px;
+    border: 2px solid #ddd;
+    border-radius: 5px;
+}
+.task-form button {
+    background: #28a745;
+    color: white;
+    border: none;
+    padding: 8px 15px;
+    cursor: pointer;
+    border-radius: 5px;
+}
+.task-form button:hover{
+    background: #218838;
+}
+</style>
