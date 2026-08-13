b857f56c2fb67f124ea9a28a6d2adeb7ae0bee51
kiyan rahmati <kiyan.rahmati.ss@gmail.com>
2026-08-13 14:38:55 +0330

    Add files via upload

---
+++ vue-project/src/TaskList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TaskItem from './TaskItem.vue';
+
+</script>
+
+<template >
+    <ul class="task-list">
+        <TaskItem/>
+    </ul>
+</template>
+
+<style>
+.task-list {
+    list-style: none;
+    padding: 0;
+}
+</style>
